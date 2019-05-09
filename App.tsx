bfd3905eef4e31b0a3adc2feab23e762b5464211
robrze <robert.rzepnikowski@o2.pl>
2019-05-09 14:22:07 +0200

    creating Home and Quiz components

--- App.tsx
+++ App.tsx
@@ -1,51 +1,78 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
+@format
+**/
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-interface Props {}
-export default class App extends Component<Props> {
+export default class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to the Trivia Challenge!</Text>
+        <Text style={styles.description}>
+          You will be presented with 10 True or False questions.
+        </Text>
+        <Text style={styles.description}>Can you score 100%?</Text>
+        <Button
+          title="BEGIN"
+          onPress={() => this.props.navigation.navigate("Quiz")}
+        />
       </View>
     );
   }
 }
 
+class QuizScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Entertainment: Video Games</Text>
+        <Text style={styles.description}>
+          Unturned originally started as a Roblox game.
+        </Text>
+        <Text style={styles.description}>1 of 10</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Quiz: QuizScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+    marginHorizontal: 60
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginHorizontal: 30,
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "bold"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  description: {
+    fontSize: 25,
+    textAlign: "center"
+  }
 });
